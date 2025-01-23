3fbf23b1bec3939a2bce0394dae50e4b0a84cc84
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-23 23:00:24 +0000

    Add polish infobox reader and add taxonomy pairings

--- data-scraper/infobox.py
+++ data-scraper/infobox.py
@@ -2,6 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+TAXONOMY_PAIRS = {
+    "domain": "domena",
+    "kingdom": "królestwo",
+    "phylum": "typ",
+    "class": "gromada",
+    "infraclass": "infragromada",
+    "clade": "klad",
+    "order": "rząd",
+    "infraorder": "infrarząd",
+    "suborder": "podrząd",
+    "tribe": "plemię",
+    "family": "rodzina",
+    "subfamily": "podrodzina",
+    "genus": "rodzaj",
+    "subgenus": "podrodzaj",
+}
+
 
 def get_image(url):
     """
@@ -48,32 +65,20 @@ def get_classification(rows, start_index):
     """
     classification = {}
     for i in range(start_index, len(rows)):
-        cells = rows[i].find_all("td")
+        cells = rows[i].find_all(["td", "th"])
         key = cells[0].get_text().strip().lower().replace(":", "")
         value = cells[1].get_text().strip().lower()
-        if key == "species":
+        if key == "species" or key == "gatunek":
             return classification
         classification[key] = value
     return classification
 
 
-def read_british_animal(url):
-    """
-    Extracts information about a British animal from a given Wikipedia URL.
+def merge_animals(english, polish):
+    english["polish_name"] = polish["polish_name"]
 
-    Args:
-        url (str): The URL of the Wikipedia page containing the animal's information.
 
-    Returns:
-        None: This function prints the extracted animal information.
-
-    The extracted information includes:
-        - english_name (str): The English name of the animal.
-        - img_file (str): The URL of the animal's image.
-        - latin_name (str, optional): The Latin (binomial) name of the animal.
-        - classification (dict, optional): The scientific classification of the animal.
-        - audio (str, optional): The URL of the animal's audio file.
-    """
+def read_british_animal(url, ignore_other_language=False):
     domain = url.split("/wiki/")[0]
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     rows = soup.find("table", {"class": "infobox"}).find_all("tr")
@@ -90,4 +95,34 @@ def read_british_animal(url):
             animal["classification"] = get_classification(rows, i + 1)
         if row.find("audio") is not None:
             animal["audio"] = get_audio(f"{domain}{row.find("audio")["resource"]}")
-    print(animal)
+
+    polish_link = soup.find("a", {"lang": "pl"})["href"]
+    if not ignore_other_language and polish_link:
+        polish_animal = read_polish_animal(polish_link, True)
+
+    return animal
+
+
+def read_polish_animal(url, ignore_other_language=False):
+    domain = url.split("/wiki/")[0]
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    rows = soup.find("table", {"class": "infobox"}).find_all("tr")
+    animal = {
+        "polish_name": soup.find("table", {"class": "infobox"})
+        .find("caption")
+        .get_text()
+        .strip()
+        .lower(),
+        "latin_name": re.sub(r"\[.*\]", r"", rows[0].get_text().strip().lower()),
+        "img_file": get_image(f"{domain}{rows[3].find("a")["href"]}"),
+    }
+    for i, row in enumerate(rows):
+        if row.get_text().strip() == "Systematyka":
+            animal["classification"] = get_classification(rows, i + 1)
+
+    english_link = soup.find("a", {"lang": "en"})["href"]
+    if not ignore_other_language and english_link:
+        english_animal = read_british_animal(english_link, True)
+        print(english_animal)
+
+    return animal
