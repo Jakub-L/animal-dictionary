54ccc136cd2606718640090ab83b65a4408093aa
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-23 23:35:55 +0000

    Improve infobox reading

--- data-scraper/infobox.py
+++ data-scraper/infobox.py
@@ -67,7 +67,7 @@ def get_classification(rows, start_index):
     for i in range(start_index, len(rows)):
         cells = rows[i].find_all(["td", "th"])
         key = cells[0].get_text().strip().lower().replace(":", "")
-        value = cells[1].get_text().strip().lower()
+        value = list(cells[1].stripped_strings)[0].lower()
         if key == "species" or key == "gatunek":
             return classification
         classification[key] = value
@@ -83,13 +83,13 @@ def read_british_animal(url, ignore_other_language=False):
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     rows = soup.find("table", {"class": "infobox"}).find_all("tr")
     animal = {
-        "english_name": re.sub(r"\[.*\]", r"", rows[0].get_text().strip()),
+        "english_name": re.sub(r"\[.*\]|\n.*", r"", list(rows[0].stripped_strings)[0]),
         "img_file": get_image(f"{domain}{rows[1].find("a")["href"]}"),
     }
     for i, row in enumerate(rows):
         if row.get_text().strip() == "Binomial name":
             animal["latin_name"] = re.sub(
-                r"\(.*\)", r"", rows[i + 1].get_text().strip().lower()
+                r"\(.*\)|,.*", r"", list(rows[i + 1].stripped_strings)[0].lower()
             )
         if row.get_text().strip() == "Scientific classification":
             animal["classification"] = get_classification(rows, i + 1)
@@ -113,7 +113,7 @@ def read_polish_animal(url, ignore_other_language=False):
         .get_text()
         .strip()
         .lower(),
-        "latin_name": re.sub(r"\[.*\]", r"", rows[0].get_text().strip().lower()),
+        "latin_name": re.sub(r"\[.*\]", r"", list(rows[0].stripped_strings)[0].lower()),
         "img_file": get_image(f"{domain}{rows[3].find("a")["href"]}"),
     }
     for i, row in enumerate(rows):
--- data-scraper/main.py
+++ data-scraper/main.py
@@ -1,19 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
 from birds import get_british_bird_links, get_polish_bird_links
 from mammals import get_british_mammal_links, get_polish_mammal_links
 
-from infobox import read_british_animal
+from infobox import read_british_animal, read_polish_animal
 
 
 def main():
-    a = list(get_british_bird_links().values())[0]
-    # print(get_polish_bird_links())
-    b = list(get_british_mammal_links().values())[0]
-    # print(get_polish_mammal_links())
-    read_british_animal(a)
+    a = list(get_british_bird_links().values())
+    b = list(get_british_mammal_links().values())
+    c = list(get_polish_bird_links().values())[0]
+    d = list(get_polish_mammal_links().values())[0]
+
+    # print(read_british_animal("https://en.wikipedia.org/wiki/Eurasian_lynx"))
+
+    for url in a + b:
+        try:
+            animal = read_british_animal(url)
+            with open("animals.json", "a") as f:
+                f.write(json.dumps(animal) + "\n")
+        except:
+            print(url)
 
 
 if __name__ == "__main__":
