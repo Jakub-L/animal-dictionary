16991f84bab6db29dc2a3c8f790ac4512564aff8
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-25 22:18:18 +0000

    Add functionality to build data JSON and download files

--- data-scraper/main.py
+++ data-scraper/main.py
@@ -1,29 +1,63 @@
 import requests
-from bs4 import BeautifulSoup
-import re
 import json
+from pathlib import Path
 
 from birds import get_british_bird_links, get_polish_bird_links
 from mammals import get_british_mammal_links, get_polish_mammal_links
+from infobox import read_animal
 
-from infobox import read_british_animal, read_polish_animal
+
+def kebab_case(string):
+    return string.lower().replace(" ", "-")
+
+
+def get_file(url, base_path):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    extension = Path(url).suffix
+    response = requests.get(url, headers=headers)
+    with open(f"{base_path}{extension}", "wb") as file:
+        file.write(response.content)
 
 
 def main():
-    a = list(get_british_bird_links().values())
-    b = list(get_british_mammal_links().values())
-    c = list(get_polish_bird_links().values())[0]
-    d = list(get_polish_mammal_links().values())[0]
+    urls = (
+        get_british_bird_links()
+        + get_british_mammal_links()
+        + get_polish_bird_links()
+        + get_polish_mammal_links()
+    )
 
-    # print(read_british_animal("https://en.wikipedia.org/wiki/Eurasian_lynx"))
+    Path("../static").mkdir(exist_ok=True)
+    Path("../static/audio").mkdir(exist_ok=True)
+    Path("../static/images").mkdir(exist_ok=True)
 
-    for url in a + b:
-        try:
-            animal = read_british_animal(url)
-            with open("animals.json", "a") as f:
-                f.write(json.dumps(animal) + "\n")
-        except:
-            print(url)
+    with open("data.json", "w") as file:
+        file.write("[\n")
+        for url in urls:
+            try:
+                animal = read_animal(url)
+                raw_json = {
+                    "englishName": animal["english_name"],
+                    "polishName": animal["polish_name"],
+                    "latinName": animal["latin_name"],
+                    "classification": animal["classification"],
+                }
+                file.write(json.dumps(raw_json, indent=4))
+                file.write(",\n")
+                if "img_file" in animal:
+                    get_file(
+                        animal["img_file"],
+                        f"../static/images/{kebab_case(animal['latin_name'])}",
+                    )
+                if "audio" in animal:
+                    get_file(
+                        animal["audio"],
+                        f"../static/audio/{kebab_case(animal['latin_name'])}",
+                    )
+            except Exception as e:
+                print(f"Error reading {url}: {e}")
+                continue
+        file.write("]")
 
 
 if __name__ == "__main__":
