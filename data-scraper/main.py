4307603b8f19f691a39426028c325094391e8fa2
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-25 22:58:43 +0000

    Stop downloading files due to size and group animals into files

--- data-scraper/main.py
+++ data-scraper/main.py
@@ -1,4 +1,3 @@
-import requests
 import json
 from pathlib import Path
 
@@ -7,57 +6,37 @@ from mammals import get_british_mammal_links, get_polish_mammal_links
 from infobox import read_animal
 
 
-def kebab_case(string):
-    return string.lower().replace(" ", "-")
-
-
-def get_file(url, base_path):
-    headers = {"User-Agent": "Mozilla/5.0"}
-    extension = Path(url).suffix
-    response = requests.get(url, headers=headers)
-    with open(f"{base_path}{extension}", "wb") as file:
-        file.write(response.content)
-
-
-def main():
-    urls = (
-        get_british_bird_links()
-        + get_british_mammal_links()
-        + get_polish_bird_links()
-        + get_polish_mammal_links()
-    )
-
-    Path("../static").mkdir(exist_ok=True)
-    Path("../static/audio").mkdir(exist_ok=True)
-    Path("../static/images").mkdir(exist_ok=True)
-
-    with open("data.json", "w") as file:
+def process_group(group, output_name):
+    processed_animals = set()
+    with open(f"../static/data/{output_name}.json", "w") as file:
         file.write("[\n")
-        for url in urls:
+        for url in group:
             try:
                 animal = read_animal(url)
+                if animal["latin_name"] in processed_animals:
+                    continue
+                processed_animals.add(animal["latin_name"])
                 raw_json = {
                     "englishName": animal["english_name"],
                     "polishName": animal["polish_name"],
                     "latinName": animal["latin_name"],
                     "classification": animal["classification"],
+                    "imageSrc": animal["img_file"] if "img_file" in animal else None,
+                    "audioSrc": animal["audio"] if "audio" in animal else None,
                 }
                 file.write(json.dumps(raw_json, indent=4))
                 file.write(",\n")
-                if "img_file" in animal:
-                    get_file(
-                        animal["img_file"],
-                        f"../static/images/{kebab_case(animal['latin_name'])}",
-                    )
-                if "audio" in animal:
-                    get_file(
-                        animal["audio"],
-                        f"../static/audio/{kebab_case(animal['latin_name'])}",
-                    )
             except Exception as e:
                 print(f"Error reading {url}: {e}")
                 continue
         file.write("]")
+
+
+def main():
+    birds = get_polish_bird_links() + get_british_bird_links()
+    mammals = get_polish_mammal_links() + get_british_mammal_links()
+    process_group(birds, "birds")
+    process_group(mammals, "mammals")
 
 
 if __name__ == "__main__":
