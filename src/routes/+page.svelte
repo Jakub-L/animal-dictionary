d36893024449ed0c704f1be9d8e83c08d2198e66
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-28 11:51:18 +0000

    Add basic animal card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import { data } from '$lib/data';
+
+	console.log(data);
 </script>
 
-<h1>Sample page</h1>
+<div class="flex flex-col gap-4 p-4">
+{#each data as animal}
+	<div class="border border-slate-950 rounded-3xl p-4 flex flex-col gap-2">
+		<img src={animal.imageSrc} alt={animal.latinName} class="rounded-2xl"/>
+		<h2 class="grid grid-cols-2">
+			<span>{animal.englishName}</span>
+			<span class="text-right border-l border-slate-950">{animal.polishName}</span>
+		</h2>
+	</div>
+{/each}
+</div>
