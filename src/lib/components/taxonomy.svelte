6b942d521153b62f31d6a5576b6fd2f6fec71e8b
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-29 15:01:30 +0000

    Add component for collapsible content showing taxonomy

---
+++ src/lib/components/taxonomy.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { plTaxonomicRanks } from '$lib/data';
+
+	// Props
+	interface Props {
+		classification: Record<string, string>;
+	}
+
+	const { classification }: Props = $props();
+</script>
+
+<div class="flex flex-wrap gap-2">
+	{#each Object.entries(classification) as [taxon, value]}
+		<div class="flex w-full flex-col rounded-full bg-gray-950/20 px-3 py-1">
+			<div class="text-2xs flex justify-between font-semibold capitalize">
+				<span>{taxon}</span>
+				<span>{plTaxonomicRanks[taxon]}</span>
+			</div>
+			<div class="flex justify-between text-sm capitalize">
+				<span>{value}</span>
+				<span>{value}</span>
+			</div>
+		</div>
+	{/each}
+</div>
--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -2,6 +2,22 @@ import birds from './birds.json';
 import mammals from './mammals.json';
 import reptiles from './reptiles.json';
 
-export const data = [
-	...birds
-];
+export const plTaxonomicRanks: Record<string, string> = {
+	domain: 'domena',
+	kingdom: 'królestwo',
+	phylum: 'typ',
+	class: 'gromada',
+	order: 'rząd',
+	family: 'rodzina',
+	genus: 'rodzaj',
+	subgenus: 'podrodzaj',
+	clade: 'klad',
+	subfamily: 'podrodzina',
+	tribe: 'plemię',
+	suborder: 'podrząd',
+	infraorder: 'infrarząd',
+	infraclass: 'infragromada',
+	'species complex': 'kompleks gatunków'
+};
+
+export const animals = [...birds, ...mammals, ...reptiles];
