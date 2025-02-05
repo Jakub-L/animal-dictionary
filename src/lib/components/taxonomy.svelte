c70ed0517dabbed049241229b0d01eb62c9d1585
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-02-05 23:17:17 +0000

    Add filter button to taxonomy

--- src/lib/components/taxonomy.svelte
+++ src/lib/components/taxonomy.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import IconFunnel from '~icons/ion/funnel-sharp';
+	import IconAdd from '~icons/ion/add-circle-sharp';
+
 	import { taxonomicRanks, taxons } from '$lib/data/data.svelte';
 
 	// Props
@@ -11,15 +14,26 @@
 
 <div class="flex flex-wrap gap-2">
 	{#each Object.entries(classification) as [taxon, value]}
-		<div class="flex w-full flex-col rounded-full bg-gray-950/20 px-3 py-1">
-			<div class="text-2xs flex justify-between font-semibold capitalize">
-				<span>{taxon}</span>
-				<span>{taxonomicRanks[taxon]}</span>
+		<div class="flex w-full gap-3 rounded-full bg-gray-950/20 py-1 pr-1 pl-3">
+			<div class="flex w-full items-center justify-between">
+				<div class="flex flex-col">
+					<span class="text-2xs font-semibold capitalize">{taxon}</span>
+					<span class="text-sm capitalize">{value}</span>
+				</div>
+				<div class="flex flex-col items-end">
+					<span class="text-2xs font-semibold capitalize">{taxonomicRanks[taxon]}</span>
+					<span class="text-sm capitalize">{taxons[value] ?? 'Error'}</span>
+				</div>
 			</div>
-			<div class="flex justify-between text-sm capitalize">
-				<span>{value}</span>
-				<span>{taxons[value] ?? 'Error'}</span>
-			</div>
+			<button
+				class="relative flex min-h-12 min-w-12 items-center justify-center rounded-full border border-gray-400 p-0.5 text-gray-700 hover:bg-gray-400 focus-visible:outline-4 focus-visible:-outline-offset-1 focus-visible:outline-gray-400 active:bg-gray-500 print:hidden"
+				title="Add as filter"
+			>
+				<IconFunnel class="mt-2 -ml-1 h-5 w-5" />
+				<div class="absolute top-2.5 right-2 rounded-full bg-gray-50">
+					<IconAdd class="h-4 w-4" />
+				</div>
+			</button>
 		</div>
 	{/each}
 </div>
