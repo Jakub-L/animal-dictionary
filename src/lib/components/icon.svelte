b2f4d983f75c7544d42213b275f66b67d745dadb
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-02-06 22:04:30 +0000

    Add icon component

---
+++ src/lib/components/icon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		id: string;
+		class: string;
+		alt: string;
+	}
+
+	const { id, class: className, alt }: Props = $props();
+</script>
+
+<svg role="img" class={className} aria-label={alt} width="32" height="32" viewBox="0 0 32 32">
+	<use href={`/icons/${id}.svg#${id}`} />
+</svg>
